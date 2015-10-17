03d41139623d263afa385baf2da6ed135c2d9a72
Bryan Baraoidan <bryan.baraoidan@gmail.com>
2015-10-17 16:26:22 -0700

    Added a triangle to the objects rendered in multi-render

--- render-multiple-objects.lisp
+++ render-multiple-objects.lisp
@@ -43,14 +43,27 @@
                                                                            0.5  0.5 0.0 0.0 1.0
                                                                            -0.5 0.5 1.0 1.0 0.0
                                                                            -0.5 -0.5 1.0 0.0 0.0)))
-                 (square-translate-scale-matrix (sb-cga:matrix* (sb-cga:translate* 400.0 300.0 0.0)
-                                                         (sb-cga:scale* 30.0 30.0 0.0))))
+                 (square-translate-scale-matrix (sb-cga:matrix* (sb-cga:translate* 200.0 300.0 0.0)
+                                                                (sb-cga:scale* 50.0 50.0 0.0)))
+                 (triangle-vao (make-instance 'kit.gl.vao:vao :type 'flat-vao))
+                 (triangle-vertex-color-data (make-array 15
+                                                         :element-type 'single-float
+                                                         :initial-contents '(-0.5 -0.5 1.0 0.0 0.0
+                                                                             0.5 -0.5 0.0 1.0 0.0
+                                                                             0.0 0.5 0.0 0.0 1.0)))
+                 (triangle-translate-scale-matrix (sb-cga:matrix* (sb-cga:translate* 600.0 300.0 0.0)
+                                                                  (sb-cga:scale* 50.0 50.0 0.0))))
             (kit.gl.shader:use-program shader :basic-shader)
 
             (kit.gl.vao:vao-buffer-vector square-vao
                                           0
                                           (* 4 (length square-vertex-color-data))
                                           square-vertex-color-data)
+
+            (kit.gl.vao:vao-buffer-vector triangle-vao
+                                          0
+                                          (* 4 (length triangle-vertex-color-data))
+                                          triangle-vertex-color-data)
 
             
             (with-event-loop (:method :poll)
@@ -64,6 +77,13 @@
                      (kit.gl.vao:vao-draw square-vao
                                           :primitive :triangles
                                           :count 6)
+                     (kit.gl.shader:uniform-matrix shader
+                                                   :model-view-projection-matrix
+                                                   4
+                                                   (vector (sb-cga:matrix* projection-matrix triangle-translate-scale-matrix)))
+                     (kit.gl.vao:vao-draw triangle-vao
+                                          :primitive :triangles
+                                          :count 3)
                      (gl-swap-window window))
               (:quit (kit.gl.vao:vao-unbind square-vao)
                      t))))))))
